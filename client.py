aba98718dafa4b58475b2301a5d25968225b2985
Dexter Yan <maildexteryan@gmail.com>
2025-03-25 01:47:30 -0400

    adding initial client and server code

---
+++ client.py
@@ -0,0 +1,66 @@
+import grpc
+import chatservice_pb2
+import chatservice_pb2_grpc
+from google.protobuf.empty_pb2 import Empty
+
+import sys
+import threading
+
+class ChatClient:
+    def __init__(self, username):
+        self.channel = grpc.insecure_channel('localhost:50051')
+        self.stub = chatservice_pb2_grpc.ChatServiceStub(self.channel)
+        self.username = username
+        self.stop_event = threading.Event()
+        self.receive_thread = None
+        self.input_buffer = ""
+
+    def send_message(self, message):
+        message_request = chatservice_pb2.MessageRequest(username=self.username, message=message)
+        self.stub.SendMessage(message_request)
+
+    def receive_messages(self):
+        try:
+            for message in self.stub.ChatStream(Empty()):
+                if self.stop_event.is_set():
+                    break
+                
+                sys.stdout.write('\r' + ' ' * (len(self.input_buffer) + 2))
+                sys.stdout.write('\r')
+                print(f"{message.username}: {message.message}")
+ 
+                sys.stdout.write(f"> {self.input_buffer}")
+                sys.stdout.flush()
+        except grpc.RpcError:
+            print("Connection to server lost")
+
+    def start_chat(self):
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.daemon = True
+        receive_thread.start()
+        try:
+            while True:
+                self.input_buffer = input("> ")
+                
+                if self.input_buffer.lower() == 'quit':
+                    print("\nExiting chat")
+                    break
+                
+                self.send_message(self.input_buffer)
+                self.input_buffer = ""
+
+        except KeyboardInterrupt:
+            print("\nExiting chat")
+        finally:
+            self.stop_event.set()
+            self.channel.close()
+            sys.exit(0)
+
+if __name__ == "__main__":
+    try:
+        username = input("Enter your username: ")
+        client = ChatClient(username)
+        client.start_chat()
+    except KeyboardInterrupt:
+        print("\nChat client terminated.")
+        sys.exit(0)
---
+++ server.py
@@ -0,0 +1,74 @@
+import grpc
+import chatservice_pb2
+import chatservice_pb2_grpc
+
+from concurrent import futures
+from pymongo import MongoClient
+import time
+
+class ChatService(chatservice_pb2_grpc.ChatServiceServicer):
+    def __init__(self):
+        # initialize mongodb service, currently on local
+        self.mongo_client = MongoClient('localhost', 27017)
+        self.db = self.mongo_client.chat_db
+
+    def ChatStream(self, request_iterator, context):
+        lastindex = 0
+
+        while True:
+            while len(self.get_chat_history()) > lastindex:
+                message = self.get_chat_history()[lastindex]
+                lastindex += 1
+                yield message
+
+    def SendMessage(self, request):
+        self.save_history(request)
+        print(f"{request.username}: {request.message}")
+
+        return chatservice_pb2.MessageResponse(username=request.username, message=request.message)
+
+    def save_history(self, request):
+        #mongodb insert 
+        message_doc = {
+            'message': request.message,
+            'user': request.username,
+        }
+
+        self.db.messages.insert_one(message_doc)
+
+    def get_chat_history(self):
+        chat_history = self.db.messages.find()
+
+        message_list = [chatservice_pb2.MessageResponse(
+            username=message['user'],
+            message=message['message'],
+        ) for message in chat_history]
+
+        return message_list
+
+    def print_previous_history(self):
+        previous_history = self.get_chat_history()
+        print("Previous Chat History:")
+        for message in previous_history:
+            print(f"{message.username}: {message.message}")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chat_service = ChatService()
+    chatservice_pb2_grpc.add_ChatServiceServicer_to_server(chat_service, server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+
+    # chat_service.print_previous_history()
+
+    print("Server started")
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        print("\nClosing server")
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
